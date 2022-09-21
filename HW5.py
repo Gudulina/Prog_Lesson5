d769453864b03ea82d35b4f6314b1580aefaa3f2
Elena Savinykh <gudulina@mail.ru>
2022-09-21 22:02:07 +0700

    Add task 4

--- HW5.py
+++ HW5.py
@@ -164,61 +164,102 @@
 
 # 3. Создайте программу для игры в ""Крестики-нолики"".
 
-field = list (range(1,10))
+# field = list (range(1,10))
 
-def draw_field(field):
-    print ("-" * 13)
-    for i in range(3):
-        print ("|", field[0+i*3], "|", field[1+i*3], "|", field[2+i*3], "|")
-        print ("-" * 13)
+# def draw_field(field):
+#     print ("-" * 13)
+#     for i in range(3):
+#         print ("|", field[0+i*3], "|", field[1+i*3], "|", field[2+i*3], "|")
+#         print ("-" * 13)
 
-def take_input(player_token):
-    valid = False
-    while not valid:
-        player_answer = input("Введите число от 1 до 9, куда поставить " + player_token + ": ")
-        try:
-            player_answer = int(player_answer)
-        except:
-            print ("Некорректный ввод. Введите число от 1 до 9.")
-            continue
-        if player_answer >= 1 and player_answer <= 9:
-            if (str(field[player_answer-1]) not in "XO"):
-                field[player_answer-1] = player_token
-                valid = True
-            else:
-                print ("Клетка занята.")
-        else:
-            print ("Введите число от 1 до 9.")
+# def take_input(player_token):
+#     valid = False
+#     while not valid:
+#         player_answer = input("Введите число от 1 до 9, куда поставить " + player_token + ": ")
+#         try:
+#             player_answer = int(player_answer)
+#         except:
+#             print ("Некорректный ввод. Введите число от 1 до 9.")
+#             continue
+#         if player_answer >= 1 and player_answer <= 9:
+#             if (str(field[player_answer-1]) not in "XO"):
+#                 field[player_answer-1] = player_token
+#                 valid = True
+#             else:
+#                 print ("Клетка занята.")
+#         else:
+#             print ("Введите число от 1 до 9.")
 
-def who_win(field):
-    win_variants = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-    for each in win_variants:
-        if field[each[0]] == field[each[1]] == field[each[2]]:
-            return field[each[0]]            
-    return False
+# def who_win(field):
+#     win_variants = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+#     for each in win_variants:
+#         if field[each[0]] == field[each[1]] == field[each[2]]:
+#             return field[each[0]]            
+#     return False
 
-def game (field):
-    draw_field(field)
-    counter = 0
-    win = False
-    while not win:        
-        if counter % 2 == 0:
-            take_input("X")
-        else:
-            take_input("O")
-        counter += 1
+# def game (field):
+#     draw_field(field)
+#     counter = 0
+#     win = False
+#     while not win:        
+#         if counter % 2 == 0:
+#             take_input("X")
+#         else:
+#             take_input("O")
+#         counter += 1
         
-        if counter > 4:            
-            who_win(field)
-            if who_win:
-                draw_field(field)
-                print ("Вы победили!")
-                win = True
-                break
-        if counter == 9:
-            draw_field(field)
-            print ("Ничья!")
-            break
+#         if counter > 4:            
+#             who_win(field)
+#             if who_win:
+#                 draw_field(field)
+#                 print ("Вы победили!")
+#                 win = True
+#                 break
+#         if counter == 9:
+#             draw_field(field)
+#             print ("Ничья!")
+#             break
+#         else:
+#             draw_field(field)
+# game (field)
+
+
+
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+# Сжатие данных
+def rle_compression(data):
+    encoding = ''
+    prev_char = ''
+    count = 1
+    if not data: return ''
+    for char in data:
+        if char != prev_char: 
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
         else:
-            draw_field(field)
-game (field)
+            count += 1
+    else: 
+        encoding += str(count) + prev_char
+        return encoding
+
+# Восстановление данных
+
+def recovery(data):
+    decode = ''
+    count = ''
+    for char in data:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+a = rle_compression('eeeeeeeeellllllllllllllllleeeeeeeeennnnnnaaaaaaaaa')
+print(a)
+
+b = recovery('9e17l9e6n9a')
+print(b)
